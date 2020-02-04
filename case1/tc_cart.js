4d92513be52891d901bedebca2784c6c0afc7a02
Yelaina <ybuford201@west-mec.org>
2020-02-03 18:25:13 -0700

    case 1 step 6

---
+++ case1/tc_cart.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: yelaina buford
+   Date:   2/3/20
+   
+   Filename: tc_cart.js
+	
+*/
+
+var item = ["10582", "23015", "41807", "10041"];
+var itemDescription = ["10582", "23015", "41807"];
--- case1/tc_order.js
+++ case1/tc_order.js
@@ -5,8 +5,8 @@
    Tutorial 10
    Case Problem 1
   
-   Author: 
-   Date:   
+   Author: yelaina buford
+   Date:   2/3/20
      
    Filename: tc_order.js  
 
